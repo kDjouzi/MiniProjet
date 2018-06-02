5ecf7a9b7285788e4c3653844560be8fc154db9c
Khémys Djouzi <31814500+kDjouzi@users.noreply.github.com>
2018-06-02 22:32:11 +0200

    Création d'une animation JS pour la FAQ (entrée et sortie)

--- script.js
+++ script.js
@@ -1,57 +1,157 @@
 //une URL isolée, serait-ce plus efficace ?
 var url = "https://simplement-e-funcs.azurewebsites.net/api/HttpTriggerCSharp2";
 
+//initialisation de la div contenant la FAQ (invisible pour le moment)
+var div = document.getElementById("helpBox");
+div.style.right = "-300px";
+
 /* Donnez-moi ce JSON !
 Récupération et formatage du JSON avec balises HTML.
 On n'oublie pas de logger le tout !
+Téléchargement du JSON à l'ouverture de la page, pour éviter de le re-télécharger à chaque ouverture du spoiler
  */
 
-function getJSON() {
-    var xhr = new XMLHttpRequest(); //création de la requête
-    xhr.open("GET", url); //requête GET pour ouvrir l'url en mode asynchrone
+var xhr = new XMLHttpRequest(); //création de la requête
+xhr.open("GET", url); //requête GET pour ouvrir l'url en mode asynchrone
 
 
-    xhr.addEventListener('readystatechange', function () { 
-        //vérification de l'état de la requête
+xhr.addEventListener('readystatechange', function () {
+    //vérification de l'état de la requête
 
-        if (xhr.readyState == 4 && xhr.status == 200) { //si la requête est bien exécutée, et que la page existe...
-           
-            console.log("La requête fonctionne !");
-            console.log("JSON dans if : " + xhr.response);
-            
-            var json = JSON.parse(xhr.response); //...on parse le JSON dans une variable faite pour ça...
+    if (xhr.readyState == 4 && xhr.status == 200) { //si la requête est bien exécutée, et que la page existe...
 
-            console.log("var json = " + json);
-            
-            var htm = ""; //stockage de code html à injecter
-            
-            //On parcourt le tableau du début à la fin, en insérant les données JSON dans les balises appropriées
-            for (var i = 0; i < json.length; i++) { 
-                htm += "<h1>" + json[i].Question + "</h1>";
-                htm += "<p>" + json[i].Reponse + "</p>";
-            }
+        console.log("La requête fonctionne !");
+        console.log("JSON dans if : " + xhr.response);
 
-            document.getElementById("helpBox").innerHTML = htm; //...Et ça part sur la page !
+        var json = JSON.parse(xhr.response); //...on parse le JSON dans une variable faite pour ça...
 
-        } else {
-            //On logge le statut et l'état de la requête, pour attraper une erreur s'il y a lieu
-            
-            console.log("code HTML : " + xhr.status);
-            console.log("Etat de la requête : " + xhr.readyState);
+        console.log("var json = " + json.toString());
+
+        var htm = ""; //stockage de code html à injecter
+
+        //On parcourt le tableau du début à la fin, en insérant les données JSON dans les balises appropriées
+        for (var i = 0; i < json.length; i++) {
+            htm += "<h1>" + json[i].Question + "</h1>";
+            htm += "<p>" + json[i].Reponse + "</p>";
         }
-    });
 
-    xhr.send(null); //on n'envoie rien
+        //document.getElementsByClassName("helpBoxContent").innerHTML = htm;
 
-    console.log("réponse : " + xhr.responseText);
-}
+        document.getElementById("helpBox").innerHTML = htm; //...Et ça part sur la page !
+
+    } else {
+        //On logge le statut et l'état de la requête, pour attraper une erreur s'il y a lieu
+
+        console.log("code HTML : " + xhr.status);
+        console.log("Etat de la requête : " + xhr.readyState);
+    }
+});
+
+xhr.send(null); //on n'envoie rien
 
 //Affichage (ou non) de la div qui contiendra les questions
 function FAQtoggle() {
 
-    getJSON();
+    //document.getElementById("helpBox").classList.toggle("backToTheLight");
 
-    document.getElementById("helpBox").classList.toggle("backToTheLight");
+    //Pour '"cliquer" sur la checkbox
+    document.getElementById("isexpanded").click();
+
+    var cb = document.getElementById("isexpanded");
+
+    FAQanimate(cb);
+    /*
+    if (cb.checked == true) {
+        console.log("isexpanded est maintenant cochée.");
+    } else {
+        console.log("isexpanded n'est plus cochée.");
+    }
+    */
+
+}
+
+function FAQanimate(cb) {
+    var div = document.getElementById("helpBox");
+    if (cb.checked == true) {
+        console.log("isexpanded est maintenant cochée.");
+
+        //script d'animation d'entrée + fade-in
+
+        //initialisation des valeurs de transparence et de position
+        var pos = -300;
+        var opacity = 0;
+
+        //créations des intervalles pour les animations
+        var mvt = setInterval(posAnimate, 10);
+        var opa = setInterval(opAnimate, 30);
+
+        //animation de position
+        function posAnimate() {
+            if (pos == 10) {
+                clearInterval(mvt);
+            } else {
+                pos += 10;
+                div.style.right = pos.toString() + "px";
+            }   
+        }
+
+        //animation de transparence
+        function opAnimate() {
+            if (opacity >= 1) {
+                clearInterval(opa);
+            } else {
+                opacity += 0.8;
+                div.style.opacity = opacity.toString();
+            }
+        }
+
+        console.log("Position : " + pos);
+        //var div = document.getElementById("helpBox");
+        console.log(opacity);
+
+        if (pos != -300 || opacity != 0) {/* ne rien faire */}
+
+        //div.style.right = "10px";
+
+    } else {
+        //script d'animation de sortie + fade-out
+
+        //initialisation des valeurs de transparence et de position
+        var pos = 10;
+        var opacity = 1;
+
+        //créations des intervalles pour les animations
+        var mvt = setInterval(posAnimate, 10);
+        var opa = setInterval(opAnimate, 30);
+
+        //animation de position
+        function posAnimate() {
+            if (pos == -300) {
+                clearInterval(mvt);
+            } else {
+                pos -= 10;
+                div.style.right = pos.toString() + "px";
+            }
+            
+        }
+
+        //animation de transparence
+        function opAnimate () {
+            if (opacity <= 0) {
+                clearInterval(opa);
+            } else {
+                opacity -= 0.08;
+                div.style.opacity = opacity.toString();
+            }
+        }
+
+        console.log(opacity);
+
+        var div = document.getElementById("helpBox");
+
+
+        console.log("isexpanded n'est plus cochée.");
+    }
 }
 
 //création de code couleur hexadécimal random
