9b663f642f34fddb77d503e3b27f989f225896d6
Khémys Djouzi <31814500+kDjouzi@users.noreply.github.com>
2018-06-02 14:01:46 +0200

    Petit ajout de CSS, simplification du tout suite aux tests
    Je commence maintenant à coder la manipulation du JSON, j'attend de trouver un style d'affichage qui me plaît avant de commit !

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var url = "https://simplement-e-funcs.azurewebsites.net/api/HttpTriggerCSharp2";
 On entre dans le réel appel de l'API, sans pour autant formater quoi que ce soit.
 Pour le moment, l'idée est de tester que tout fonctionne... On rendra ça plus joli ensuite ;)
  */
+var responseAwait = "loading...";
+
 function getJSON() {
     var xhr = new XMLHttpRequest(); //création de la requête
     xhr.open("GET", url); //requête GET pour ouvrir reqUrl en mode asynchrone
@@ -14,7 +16,7 @@ function getJSON() {
         if (xhr.readyState == 4 && xhr.status == 200) { //si la requête est bien exécutée, et que la page existe...
             console.log("La requête fonctionne !"); //...on logge le tout dans la console pour tester ça, et le script continue de s'exécuter...
             console.log("JSON dans if : " + xhr.response); //...sans oublier de logger le JSON
-            document.getElementById("helpTest").innerHTML = xhr.responseText; //test de la manipulation de HTML
+            document.getElementById("helpBoxTest").innerHTML = xhr.responseText; //test de la manipulation de HTML
         } else {
             //On logge le statut et l'état de la requête
             console.log("code HTML : " + xhr.status); 
@@ -23,4 +25,10 @@ function getJSON() {
     });
 
     xhr.send(null); //on n'envoie rien
+    console.log("réponse : " + xhr.responseText);
+}
+
+function toggle(){
+    getJSON();
+    document.getElementById("helpBoxTest").classList.toggle("backToTheLight");
 }
